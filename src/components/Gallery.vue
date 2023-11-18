8b501b59492b1c700781c67cdcc8018e85364caf
Maksym6724 <m.karpenko0001@gmail.com>
2023-11-18 13:14:39 +0200

    kursova

---
+++ src/components/Gallery.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1 class="">Gallery</h1>
+  <div class="gallery">
+    <img src="https://picsum.photos/id/1028/300/300" alt="a forest after an apocalypse">
+    <img src="https://picsum.photos/id/15/300/300" alt="a waterfall and many rocks">
+    <img src="https://picsum.photos/id/1040/300/300" alt="a house on a mountain">
+    <img src="https://picsum.photos/id/106/300/300" alt="sime pink flowers">
+    <img src="https://picsum.photos/id/136/300/300" alt="big rocks with some trees">
+    <img src="https://picsum.photos/id/1039/300/300" alt="a waterfall, a lot of tree and a great view from the sky">
+    <img src="https://picsum.photos/id/110/300/300" alt="a cool landscape">
+    <img src="https://picsum.photos/id/1047/300/300" alt="inside a town between two big buildings">
+    <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain">
+  </div>
+</template>
+
+<script>
+export default {
+name: "Gallery"
+}
+</script>
+
+<style scoped>
+.gallery {
+  --s: 150px; /* control the size */
+  --g: 10px;  /* control the gap */
+  --f: 1.5;   /* control the scale factor */
+
+  display: grid;
+  gap: var(--g);
+  width: 1024px;
+  aspect-ratio: 1;
+  grid-template-columns: repeat(3,auto);
+}
+
+.gallery > img {
+  width: 0;
+  height: 0;
+  min-height: 100%;
+  min-width: 100%;
+  object-fit: cover;
+  cursor: pointer;
+  filter: grayscale(80%);
+  transition: .35s linear;
+}
+
+.gallery img:hover{
+  filter: grayscale(0);
+  width:  calc(var(--s)*var(--f));
+  height: calc(var(--s)*var(--f));
+}
+
+
+body {
+  margin: 0;
+  min-height: 100vh;
+  display: grid;
+  place-content: center;
+  background: #60c4ff;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,307 @@
+<template>
+  <div id="carouselExample" class="carousel slide w-75">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./images/img.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./images/img.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./images/img.png" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <section class="wrapper container-fluid">
+    <div class="container-fluid align-items-center justify-content-center">
+      <div class="d-flex w-100 align-items-center justify-content-center row">
+        <h1 class="d-flex justify-content-center text-dark mb-3">News</h1>
+      </div>
+      <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+          <div class="card text-dark card-has-bg click-col"
+               style="background-image:url('https://source.unsplash.com/600x900/?tech,street');">
+            <img class="card-img d-none" src="https://source.unsplash.com/600x900/?tech,street"
+                 alt="Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?">
+            <div class="card-img-overlay d-flex flex-column">
+              <div class="card-body">
+                <small class="card-meta mb-2">Thought Leadership</small>
+                <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Web Developmet Lorem
+                  Ipsum Sit Amet Consectetur dipisi?</a></h4>
+                <small><i class="far fa-clock"></i> October 15, 2020</small>
+              </div>
+              <div class="card-footer">
+                <div class="media">
+                  <img class="mr-3 rounded-circle"
+                       src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80"
+                       alt="Generic placeholder image" style="max-width:50px">
+                  <div class="media-body">
+                    <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
+                    <small>Director of UI/UX</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+          <div class="card text-dark card-has-bg click-col"
+               style="background-image:url('https://source.unsplash.com/600x900/?tree,nature');">
+            <img class="card-img d-none" src="https://source.unsplash.com/600x900/?tree,nature"
+                 alt="Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?">
+            <div class="card-img-overlay d-flex flex-column">
+              <div class="card-body">
+                <small class="card-meta mb-2">Thought Leadership</small>
+                <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Creative Manner
+                  Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
+                <small><i class="far fa-clock"></i> October 15, 2020</small>
+              </div>
+              <div class="card-footer">
+                <div class="media">
+                  <img class="mr-3 rounded-circle"
+                       src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80"
+                       alt="Generic placeholder image" style="max-width:50px">
+                  <div class="media-body">
+                    <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
+                    <small>Director of UI/UX</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+          <div class="card text-dark card-has-bg click-col"
+               style="background-image:url('https://source.unsplash.com/600x900/?computer,design');">
+            <img class="card-img d-none" src="https://source.unsplash.com/600x900/?computer,design"
+                 alt="Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?">
+            <div class="card-img-overlay d-flex flex-column">
+              <div class="card-body">
+                <small class="card-meta mb-2">Thought Leadership</small>
+                <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Design Studio Lorem
+                  Ipsum Sit Amet Consectetur dipisi?</a></h4>
+                <small><i class="far fa-clock"></i> October 15, 2020</small>
+              </div>
+              <div class="card-footer">
+                <div class="media">
+                  <img class="mr-3 rounded-circle"
+                       src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80"
+                       alt="Generic placeholder image" style="max-width:50px">
+                  <div class="media-body">
+                    <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
+                    <small>Director of UI/UX</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+          <div class="card text-dark card-has-bg click-col"
+               style="background-image:url('https://source.unsplash.com/600x900/?tech,street');">
+            <img class="card-img d-none" src="https://source.unsplash.com/600x900/?tech,street"
+                 alt=" Lorem Ipsum Sit Amet Consectetur dipisi?">
+            <div class="card-img-overlay d-flex flex-column">
+              <div class="card-body">
+                <small class="card-meta mb-2">Thought Leadership</small>
+                <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">UI/UX Design Lorem
+                  Ipsum Sit Amet Consectetur dipisi?</a></h4>
+                <small><i class="far fa-clock"></i> October 15, 2020</small>
+              </div>
+              <div class="card-footer">
+                <div class="media">
+                  <img class="mr-3 rounded-circle"
+                       src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80"
+                       alt="Generic placeholder image" style="max-width:50px">
+                  <div class="media-body">
+                    <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
+                    <small>Director of UI/UX</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+          <div class="card text-dark card-has-bg click-col"
+               style="background-image:url('https://source.unsplash.com/600x900/?tree,nature');">
+            <img class="card-img d-none" src="https://source.unsplash.com/600x900/?tree,nature"
+                 alt="Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?">
+            <div class="card-img-overlay d-flex flex-column">
+              <div class="card-body">
+                <small class="card-meta mb-2">Thought Leadership</small>
+                <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Creative Manner
+                  Design Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
+                <small><i class="far fa-clock"></i> October 15, 2020</small>
+              </div>
+              <div class="card-footer">
+                <div class="media">
+                  <img class="mr-3 rounded-circle"
+                       src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80"
+                       alt="Generic placeholder image" style="max-width:50px">
+                  <div class="media-body">
+                    <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
+                    <small>Director of UI/UX</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+          <div class="card text-dark card-has-bg click-col"
+               style="background-image:url('https://source.unsplash.com/600x900/?computer,design');">
+            <img class="card-img d-none" src="https://source.unsplash.com/600x900/?computer,design"
+                 alt="Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?">
+            <div class="card-img-overlay d-flex flex-column">
+              <div class="card-body">
+                <small class="card-meta mb-2">Thought Leadership</small>
+                <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Creative Manner
+                  Design Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
+                <small><i class="far fa-clock"></i> October 15, 2020</small>
+              </div>
+              <div class="card-footer">
+                <div class="media">
+                  <img class="mr-3 rounded-circle"
+                       src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80"
+                       alt="Generic placeholder image" style="max-width:50px">
+                  <div class="media-body">
+                    <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
+                    <small>Director of UI/UX</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+body {
+  background: #161616
+}
+
+.lead {
+  color: #aaa
+}
+
+.wrapper {
+  margin: 10vh
+}
+
+.card {
+  border: none;
+  transition: all .5s cubic-bezier(.19, 1, .22, 1);
+  overflow: hidden;
+  border-radius: 20px;
+  min-height: 450px;
+  box-shadow: 0 0 12px 0 rgba(0, 0, 0, .2)
+}
+
+@media (max-width: 768px) {
+  .card {
+    min-height: 350px
+  }
+}
+
+@media (max-width: 420px) {
+  .card {
+    min-height: 300px
+  }
+}
+
+.card.card-has-bg {
+  transition: all .5s cubic-bezier(.19, 1, .22, 1);
+  background-size: 120%;
+  background-repeat: no-repeat;
+  background-position: center center
+}
+
+.card.card-has-bg:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: inherit;
+  -moz-filter: grayscale(100%);
+  -ms-filter: grayscale(100%);
+  -o-filter: grayscale(100%);
+  filter: grayscale(100%)
+}
+
+.card.card-has-bg:hover {
+  transform: scale(.98);
+  box-shadow: 0 0 5px -2px rgba(0, 0, 0, .3);
+  background-size: 130%;
+  transition: all .5s cubic-bezier(.19, 1, .22, 1)
+}
+
+.card.card-has-bg:hover .card-img-overlay {
+  transition: all .8s cubic-bezier(.19, 1, .22, 1);
+  background: #ffba21;
+  background: linear-gradient(0deg, rgba(255, 186, 33, .5) 0, #ffba21 100%)
+}
+
+.card .card-footer {
+  background: 0 0;
+  border-top: none
+}
+
+.card .card-footer .media img {
+  border: solid 3px rgba(255, 255, 255, .3)
+}
+
+.card .card-title {
+  font-weight: 800
+}
+
+.card .card-meta {
+  color: rgba(0, 0, 0, .3);
+  text-transform: uppercase;
+  font-weight: 500;
+  letter-spacing: 2px
+}
+
+.card .card-body {
+  transition: all .5s cubic-bezier(.19, 1, .22, 1)
+}
+
+.card:hover {
+  cursor: pointer;
+  transition: all .8s cubic-bezier(.19, 1, .22, 1)
+}
+
+.card:hover .card-body {
+  margin-top: 30px;
+  transition: all .8s cubic-bezier(.19, 1, .22, 1)
+}
+
+.card .card-img-overlay {
+  transition: all .8s cubic-bezier(.19, 1, .22, 1);
+  background: #ffba21;
+  background: linear-gradient(0deg, rgba(255, 186, 33, .3785889356) 0, #ffba21 100%)
+}
+</style>
